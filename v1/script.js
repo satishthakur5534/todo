dfa06b1dccb4228c2d174ad26b3b4723755f396c
Satish Thakur <satishthakur5534@gmail.com>
2022-07-06 14:45:36 +0530

    script

---
+++ v1/script.js
@@ -0,0 +1,75 @@
+let js = "amazing";
+if (js === "amazing") alert("Javascript is fun!");
+40 + 8 + 23 - 5;
+console.log(40 + 8 + 23 - 5);
+
+
+let firstName = "matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+let $function = "satish";
+console.log($function);
+
+
+let myFirstjob = "coder";
+let myCurrentjob = "teacher";
+console.log(myFirstjob);
+console.log(myCurrentjob);
+
+let PI = "3.14151515";
+console.log(PI);
+
+// let talk about boolean;
+
+
+let JavascriptIsFun = true;
+console.log(JavascriptIsFun);
+console.log(typeof true);
+console.log(typeof JavascriptIsFun);
+console.log(typeof "satish");
+console.log(typeof 23);
+
+// undeifined;
+// let year
+// console.log(year);
+
+// const birthyYear = 1991;
+// birthYear = 1990;
+
+// here we dont use any declaration(let, const, var)
+
+lastName = "satish";
+console.log(lastName);
+
+/////////basic operator/////////////
+
+const now = 2037;
+jonasAge = now - 1991;
+sarahaAge = now - 2018;
+console.log(jonasAge, sarahaAge);
+
+console.log(jonasAge * 2, sarahaAge / 2, 2 ** 3);
+
+
+
+firstName = "Satish";
+lastName = "Thakur";
+console.log(firstName + ' ' + lastName);
+
+
+
+
+
+// muultidimensional arrays
+
+
+var ary = [
+    ["satidh", 15, "male", "bca"],
+    ["satidh", 15, "male", "bca"],
+    ["satidh", 15, "male", "bca"],
+    ["satidh", 15, "male", "bca"],
+];
+console.log(ary);
