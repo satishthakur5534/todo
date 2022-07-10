90dac0546da4906706642434b9619117361ee494
Satish Thakur <satishthakur5534@gmail.com>
2022-07-10 16:36:22 +0530

    script add

--- v1/script.js
+++ v1/script.js
@@ -1,4 +1,4 @@
-let js = "amazing";
+/*let js = "amazing";
 if (js === "amazing") alert("Javascript is fun!");
 40 + 8 + 23 - 5;
 console.log(40 + 8 + 23 - 5);
@@ -61,15 +61,111 @@ console.log(firstName + ' ' + lastName);
 
 
 
-
+*/
 
 // muultidimensional arrays
 
 
-var ary = [
-    ["satidh", 15, "male", "bca"],
-    ["satidh", 15, "male", "bca"],
-    ["satidh", 15, "male", "bca"],
-    ["satidh", 15, "male", "bca"],
-];
-console.log(ary);
+// var ary = [
+//     ["satidh", 15, "male", "bca"],
+//     ["satidh", 15, "male", "bca"],
+//     ["satidh", 15, "male", "bca"],
+//     ["satidh", 15, "male", "bca"],
+// ];
+// console.log(ary);
+// for (var a = 0; a < 4; a++);
+// console.log(ary[0] + ' ')
+
+
+// modify array
+// var ary = ["satish", 18, "male", "bca"];
+// console.log(ary);
+// delete ary[0]; /// delete method in js.
+// console.log(ary)
+// ary[0] = "satish"; /// how to add element method.
+// console.log(ary)
+
+// coding challenge #1
+
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heihgtJohn = 1.95;
+
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heihgtJohn * heihgtJohn);
+// console.log(BMIMark, BMIJohn);
+
+
+//task 2//
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heihgtJohn = 1.76;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heihgtJohn * heihgtJohn);
+// const markHeigherBMI = BMIMark > BMIJohn;
+// console.log(markHeigherBMI);
+
+
+
+// built string templetes
+
+// const firstName = "jonas";
+// const job = "teacher";
+// const birthYear = 1991;
+// const year = 2037;
+
+// const satish = "I'm" + firstName + ',a' + (year - birthYear) + 'years old' + job + '!';
+// console.log(satish);
+
+// const age = 19;
+// const isOldEnough = age >= 18;
+// if (isOldEnough) {
+//     console.log('satish can start drive');
+// }
+// const age = 15;
+// if (age >= 18) {
+//     console.log('satish can start drive');
+
+// } else {
+//     const yearsLeft = 18 - 15;
+//     console.log("satish is too young wait another 3 years");
+// }
+
+
+// const birthYear = 2001;
+// let century;
+// if (birthYear <= 2001) {
+//     century = 21;
+
+
+// } else {
+//     century = 20;
+// }
+// console.log(century);
+
+// // type of conversion
+
+// const inputYear = '1990';
+// console.log(Number(inputYear) + 10);
+// console.log('I am' + ' 23 ' + ' years old ');
+// console.log('30' + '20' - '10');
+// console.log('20' * '2');
+
+// // // string in Number
+// // // let n = '1' + 1; // for example  '1' is a string and 1 is number 
+// // // n = n - 1;
+// // // console.log(n);
+
+
+
+// // // Boolean 
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('satish'));
