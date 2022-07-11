43830a226c0a0733cc9a4a491cf360e6c405b15c
Satish Thakur <satishthakur5534@gmail.com>
2022-07-11 13:09:34 +0530

    something add

--- v1/questions/index.js
+++ v1/questions/index.js
@@ -1,2 +1,13 @@
-const firstName = "hello world";
-console.log(firstName);
+// const firstName = "hello world";
+// console.log(firstName);
+function sqRoot(num) {
+
+    let sqrt = num * num
+
+    return sqrt
+
+
+}
+
+let sqr = sqRoot(15)
+console.log(sqr)
